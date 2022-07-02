5ec78513315c2eb92d551b8bcb537d2be6d35eea
goutamd76 <gd760508@gmail.com>
2022-07-02 10:43:02 +0530

    add script to run app

--- main.js
+++ main.js
@@ -4,11 +4,20 @@ const port = 3000
 
 app.get('/',(req,res) =>{
   res.send('Hello World')
-})
+});
 
 app.get('/about',(req,res) =>{
-  res.send(`<h1>Welcome</h1>`)
-})
+  res.send(`<h1>Welcome</h1>`);
+});
+
+app.get('/services', (req, res) => {
+  res.send("What services you expect ?");
+});
+
+
+app.get('/products', (req, res) => {
+  res.send("This page is under construction");
+});
 
 
 app.listen(port,() =>{
